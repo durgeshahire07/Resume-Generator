effd8ded8ec14032e8e23e45b9df8684adb6df87
durgeshahire07 <durgeshahire07@gmail.com>
2021-04-03 03:49:55 +0530

    added basic layout for website

---
+++ script.js
@@ -0,0 +1,29 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+$(document).ready(function(){
+   $("button").click(function(){
+     $("p").hide();
+   });
+ });
+// function profile(){
+//    document.getElementById("profile").style="color:red"
+   
+// }
+// function education(){
+//    document.getElementById("education").style="color:red"
+//    document.getElementById("personal-info").style="visibility:hidden"
+//  }
+//  function skill(){
+//    document.getElementById("skill").style="color:red"
+//  }
+//  function work(){
+//    document.getElementById("work").style="color:red"
+//  }
+//  function project(){
+//    document.getElementById("project").style="color:red"
+//  }
+//  function award(){
+//    document.getElementById("project").style="color:red"
+//  }
+//  function award(){
+//     document.getElementById("")
+//  }
